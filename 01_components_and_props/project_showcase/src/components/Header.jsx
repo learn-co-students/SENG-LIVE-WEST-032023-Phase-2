15313f5c742f35000c6c9e3b7668efe5df939b49
betalantz <betalantz@gmail.com>
2023-04-10 13:52:40 -0700

    add components and diagrams

---
+++ 01_components_and_props/project_showcase/src/components/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+
+    return (
+        <header>
+            <h1>
+                <span className="logo">//</span> Project Showcase
+            </h1>
+            <nav>
+                <button>Light Mode</button>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
---
+++ 01_components_and_props/project_showcase/src/components/ProjectListItem.jsx
@@ -0,0 +1,18 @@
+const ProjectListItem = (props) => {
+    console.log("🚀 ~ file: ProjectListItem.jsx:2 ~ ProjectListItem ~ props:", props)
+    return (
+        <li className="card">
+            {/* <figure className="image">
+                <img src={project.image} alt={`${project.name} app`} />
+            </figure>
+            <section className="details">
+                <h4>{project.name}</h4>
+                <p>{project.about}</p>
+                <p><a href={project.link}>Link</a></p>
+            </section>
+            <footer className="extra">
+                <span className="badge blue">Phase {project.phase}</span>
+            </footer> */}
+        </li>
+    )
+}
