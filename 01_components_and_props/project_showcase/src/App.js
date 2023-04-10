356f08b1767c07b59f5159edb1658afd9f1c77bb
betalantz <betalantz@gmail.com>
2023-04-10 14:14:31 -0700

    reset branch; remove components

--- 01_components_and_props/project_showcase/src/App.js
+++ 01_components_and_props/project_showcase/src/App.js
@@ -1,14 +1,7 @@
-import Header from './components/Header'
-import ProjectForm from './components/ProjectForm'
-import ProjectList from './components/ProjectList'
-
 function App() {
   return (
     <div className="App">
-      <Header />
-      {/* above is = to calling the function Header({user: "Lantz"}) */}
-      <ProjectForm />
-      <ProjectList />
+      Project Showcase
     </div>);
 }
 
--- 01_components_and_props/project_showcase/src/components/ProjectForm.jsx
+++
@@ -1,9 +0,0 @@
-const ProjectForm = () => {
-    return(
-        <form className="form">
-            <h2>Add Project</h2>
-        </form>
-    )
-}
-
-export default ProjectForm
